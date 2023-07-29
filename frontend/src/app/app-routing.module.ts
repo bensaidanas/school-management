39b92156ace5baf89a136ea8dd0588a09f846c40
Anas Bensaid <bensaidanass9@gmail.com>
2023-07-29 12:28:01 +0100

    Adding the sidebar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentComponent } from './pages/student/student.component';
+import { ClassroomComponent } from './pages/classroom/classroom.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'student', component: StudentComponent},
+  {path: 'teacher', component: TeacherComponent},
+  {path: 'class', component: ClassroomComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { StudentComponent } from './pages/student/student.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
+import { ClassroomComponent } from './pages/classroom/classroom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    StudentComponent,
+    TeacherComponent,
+    ClassroomComponent
   ],
   imports: [
     BrowserModule,
