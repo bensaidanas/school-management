54b42ccedee2edba08e3ae3f456a5d2ef394ac22
Anas Bensaid <bensaidanass9@gmail.com>
2023-07-31 12:27:34 +0100

    Adding grade and major interfaces

---
+++ frontend/src/app/models/grade.ts
@@ -0,0 +1,8 @@
+export enum Grade {
+    // FirstYear = 'Tronc Commun',
+    // SecondYear= 'Première année Bac',
+    // ThirdYear= 'Deuxième année Bac'
+    'Tronc Commun',
+    'Première année Bac',
+    'Deuxième année Bac'
+}
---
+++ frontend/src/app/models/major.ts
@@ -0,0 +1,5 @@
+export enum Major {
+    SVT = 'Sciences de la Vie et de la Terre',
+    SM = 'Sciences Mathématiques',
+    SPC = 'Sciences Physiques et Chimiques'
+}
